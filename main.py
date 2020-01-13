4da733a375e345537e520f3fbb33d1f35dcdf019
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-01-13 11:05:53 -0500

    Add logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import yaml
+import logging
 #import wx
 
 #app = wx.App()
@@ -8,11 +9,12 @@ import yaml
 #app.MainLoop()
 
 fruits_list = []
+logging.basicConfig(level=logging.debug) # Set the logging level
 
 def find_aisle(search_item):
     for aisle in fruits_list["aisle"]:
         for item in aisle:
-            print(item)
+            logging.debug(item)
             if search_item == item :
                 return aisle
     return None
@@ -21,34 +23,34 @@ def find_aisle(search_item):
 with open(r'data/stores.yaml') as file:
     fruits_list = yaml.safe_load(file)
 
-    print(fruits_list)
-    print(fruits_list["aisle"]["1"])
+    #logging.debug(fruits_list)
+    #logging.debug(fruits_list["aisle"]["1"])
     list_of_all_things_in_isle_1 = fruits_list["aisle"]["1"]
 
-print(find_aisle("beans"))
+logging.info(find_aisle("beans"))
+if __name__ == "__main__": # Run only if this is the main file
+    #This sets the store location, which has different items in different locations.
+    count = False
+    store = input("Where are you shopping?: ")
+    while count == False:
+        #if store == "Hannaford" or store == "hannaford":
+        if store.upper() == "hannaford".upper():
+            store_items = []
+            aisle_location = []
+            count = True
+        elif "sam".upper() in store.upper():
+            store_items = []
+            aisle_location = []
+            count = True
+        else:
+            print ("Sorry, I don't recognize that store, try again.")
+            store = input("Where are you shopping?: ")
 
-#This sets the store location, which has different items in different locations.
-count = False
-store = input("Where are you shopping?: ")
-while count == False:
-    #if store == "Hannaford" or store == "hannaford":
-    if store.upper() == "hannaford".upper():
-        store_items = []
-        aisle_location = []
-        count = True
-    elif "sam".upper() in store.upper():
-        store_items = []
-        aisle_location = []
-        count = True
-    else:
-        print ("Sorry, I don't recognize that store, try again.")
-        store = input("Where are you shopping?: ")
-
-#This sets the items in the shopping list for comparison and sorting.
-list_count = int(input("How many items are on your shopping list?: "))
-shopping_list = []
-shopping_list_final = []
-for i in range(list_count):
-    item = input("Enter item: ")
-    shopping_list.append(item)
+    #This sets the items in the shopping list for comparison and sorting.
+    list_count = int(input("How many items are on your shopping list?: "))
+    shopping_list = []
+    shopping_list_final = []
+    for i in range(list_count):
+        item = input("Enter item: ")
+        shopping_list.append(item)
 
